37e3f28a8da0893aa89d063c733c009d8a4a4f4f
HarshitNegi-A <harshitnegi226@gmail.com>
2024-10-12 23:34:05 +0530

    header

---
+++ Layout/Header.js
@@ -0,0 +1,22 @@
+import "./Header.module.css"
+import React from "react"
+import measlImage from "../../assets/meals.jpg"
+import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton"
+
+const Header=()=>{
+    return(
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <button><HeaderCartButton /></button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={measlImage} alt="A table full of foods" />
+            </div>
+        </React.Fragment>
+        
+    )
+}
+
+export default Header;
