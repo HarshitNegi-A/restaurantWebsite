586a2932a3cdac114b69dc4f048c0ad55c89b179
HarshitNegi-A <harshitnegi226@gmail.com>
2024-10-13 22:46:58 +0530

    cartOpenAndClose

--- Layout/Header.js
+++ Layout/Header.js
@@ -4,12 +4,12 @@ import measlImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css"
 import HeaderCartButton from "./HeaderCartButton"
 
-const Header=()=>{
+const Header=(props)=>{
     return(
         <React.Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button><HeaderCartButton /></button>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={measlImage} alt="A table full of foods" />
